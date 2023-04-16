ac42889aeca12101e3584902cf3bc74ba61e5ab5
Timo Unger <dodo92@gmx.de>
2023-04-16 10:59:46 +0200

    Pages v1.0.0

---
+++ docs/help.js
@@ -0,0 +1,61 @@
+var help =
+[
+    [ "General", "general.html", [
+      [ "General", "general.html#autotoc_md52", [
+        [ "Features", "general.html#autotoc_md54", null ],
+        [ "Contact", "general.html#autotoc_md56", null ]
+      ] ]
+    ] ],
+    [ "Configuration", "configuration.html", [
+      [ "Configuration", "configuration.html#autotoc_md39", [
+        [ "1. Set User", "configuration.html#autotoc_md40", null ],
+        [ "2. Set Articles", "configuration.html#autotoc_md41", null ],
+        [ "3. Select Printer", "configuration.html#autotoc_md42", null ],
+        [ "4. Select Paper Width", "configuration.html#autotoc_md43", null ],
+        [ "5. Select SmartCard Reader", "configuration.html#autotoc_md44", null ],
+        [ "6. Restart und Clear", "configuration.html#autotoc_md45", null ],
+        [ "Optional Settings", "configuration.html#autotoc_md47", [
+          [ "Select Output Path", "configuration.html#autotoc_md48", null ],
+          [ "Sound", "configuration.html#autotoc_md49", null ],
+          [ "Theme", "configuration.html#autotoc_md50", null ],
+          [ "Log Verbosity", "configuration.html#autotoc_md51", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "User", "user.html", [
+      [ "User", "user.html#autotoc_md68", [
+        [ "Configuration → User", "user.html#autotoc_md69", null ]
+      ] ]
+    ] ],
+    [ "Articles", "articles.html", [
+      [ "Articles", "articles.html#autotoc_md37", [
+        [ "Configuration → Articles", "articles.html#autotoc_md38", null ]
+      ] ]
+    ] ],
+    [ "Report", "report.html", [
+      [ "Report", "report.html#autotoc_md62", [
+        [ "Show Status", "report.html#autotoc_md63", null ],
+        [ "Open Folder", "report.html#autotoc_md64", null ],
+        [ "Combin Report", "report.html#autotoc_md65", null ],
+        [ "Create Report", "report.html#autotoc_md67", null ]
+      ] ]
+    ] ],
+    [ "Printer", "printer.html", [
+      [ "Printer", "printer.html#autotoc_md57", [
+        [ "EPSON TM-T20III", "printer.html#autotoc_md58", null ],
+        [ "AXIOHM A793-4205 (with Manhattan Converter)", "printer.html#autotoc_md59", null ]
+      ] ]
+    ] ],
+    [ "SmartCard", "reader.html", [
+      [ "SmartCard Reader", "reader.html#autotoc_md60", [
+        [ "ACR1252U", "reader.html#autotoc_md61", null ]
+      ] ]
+    ] ],
+    [ "Webserver", "webserver.html", [
+      [ "Webserver", "webserver.html#autotoc_md70", [
+        [ "Description", "webserver.html#autotoc_md71", null ],
+        [ "Color codes", "webserver.html#autotoc_md72", null ],
+        [ "How to create a configuration file", "webserver.html#autotoc_md73", null ]
+      ] ]
+    ] ]
+];
---
+++ docs/navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "BonPrinter", "index.html", [
+    [ "BonPrinter", "index.html#autotoc_md31", [
+      [ "Über ℹ️", "index.html#autotoc_md32", null ],
+      [ "Funktionalitäten 🥇", "index.html#autotoc_md33", null ],
+      [ "Download ☁️ ⬇️", "index.html#autotoc_md34", null ],
+      [ "Bedienung 👆️", "index.html#autotoc_md35", null ],
+      [ "Feedback / Bugmeldung 🐞", "index.html#autotoc_md36", null ]
+    ] ],
+    [ "Changelog", "_c_h_a_n_g_e_l_o_g.html", [
+      [ "BonPrinter changelog 📋️", "_c_h_a_n_g_e_l_o_g.html#autotoc_md0", [
+        [ "[v1.0.0]", "_c_h_a_n_g_e_l_o_g.html#autotoc_md1", null ]
+      ] ]
+    ] ],
+    [ "Presentation", "_presentation.html", [
+      [ "Vorstellung BonPrinter", "_presentation.html#autotoc_md2", [
+        [ "Rechtliches 👨‍⚖️", "_presentation.html#autotoc_md3", null ],
+        [ "Pflichten ⚖️", "_presentation.html#autotoc_md4", null ],
+        [ "Bezahlung 💰", "_presentation.html#autotoc_md5", null ],
+        [ "Anforderungen an das Bestellsystem 📑", "_presentation.html#autotoc_md6", null ],
+        [ "BonPrinter Tool 🔧", "_presentation.html#autotoc_md7", [
+          [ "Erforderliche Komponenten", "_presentation.html#autotoc_md8", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "License", "_l_i_c_e_n_s_e.html", [
+      [ "GNU General Public License", "_l_i_c_e_n_s_e.html#autotoc_md9", [
+        [ "Preamble", "_l_i_c_e_n_s_e.html#autotoc_md10", null ],
+        [ "TERMS AND CONDITIONS", "_l_i_c_e_n_s_e.html#autotoc_md11", [
+          [ "0. Definitions", "_l_i_c_e_n_s_e.html#autotoc_md12", null ],
+          [ "1. Source Code", "_l_i_c_e_n_s_e.html#autotoc_md13", null ],
+          [ "2. Basic Permissions", "_l_i_c_e_n_s_e.html#autotoc_md14", null ],
+          [ "3. Protecting Users' Legal Rights From Anti-Circumvention Law", "_l_i_c_e_n_s_e.html#autotoc_md15", null ],
+          [ "4. Conveying Verbatim Copies", "_l_i_c_e_n_s_e.html#autotoc_md16", null ],
+          [ "5. Conveying Modified Source Versions", "_l_i_c_e_n_s_e.html#autotoc_md17", null ],
+          [ "6. Conveying Non-Source Forms", "_l_i_c_e_n_s_e.html#autotoc_md18", null ],
+          [ "7. Additional Terms", "_l_i_c_e_n_s_e.html#autotoc_md19", null ],
+          [ "8. Termination", "_l_i_c_e_n_s_e.html#autotoc_md20", null ],
+          [ "9. Acceptance Not Required for Having Copies", "_l_i_c_e_n_s_e.html#autotoc_md21", null ],
+          [ "10. Automatic Licensing of Downstream Recipients", "_l_i_c_e_n_s_e.html#autotoc_md22", null ],
+          [ "11. Patents", "_l_i_c_e_n_s_e.html#autotoc_md23", null ],
+          [ "12. No Surrender of Others' Freedom", "_l_i_c_e_n_s_e.html#autotoc_md24", null ],
+          [ "13. Use with the GNU Affero General Public License", "_l_i_c_e_n_s_e.html#autotoc_md25", null ],
+          [ "14. Revised Versions of this License", "_l_i_c_e_n_s_e.html#autotoc_md26", null ],
+          [ "15. Disclaimer of Warranty", "_l_i_c_e_n_s_e.html#autotoc_md27", null ],
+          [ "16. Limitation of Liability", "_l_i_c_e_n_s_e.html#autotoc_md28", null ],
+          [ "17. Interpretation of Sections 15 and 16", "_l_i_c_e_n_s_e.html#autotoc_md29", null ]
+        ] ],
+        [ "How to Apply These Terms to Your New Programs", "_l_i_c_e_n_s_e.html#autotoc_md30", null ]
+      ] ]
+    ] ],
+    [ "Help", "help.html", "help" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_c_h_a_n_g_e_l_o_g.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
